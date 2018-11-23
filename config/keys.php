fd394073f2bed94f355db6dbd970dd6fba3ddd82
Martin <martin.d@live.se>
2018-11-23 21:44:04 +0000

    updated config files

--- config/keys.php
+++ config/keys.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * Config-file for Ip & Weather app.
- * Change the values to ur own keys
+ * NOTE:
+ *      Change the values to ur own key (This key wont work)
  */
 return [
-    "darksky" => "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "darksky" => "48fbf9f35f861442c7c28f874d59338d",
 ];
